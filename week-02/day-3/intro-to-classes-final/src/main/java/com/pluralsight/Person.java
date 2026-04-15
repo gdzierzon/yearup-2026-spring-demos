2cdaf1e4a35cfd241b21696d55de766cd3d7bc91
Gregor Dzierzon <gregor@fullstackdevs.net>
2026-04-15 14:27:19 -0600

    Updated Classes Demo - with constructors

--- week-02/day-3/intro-to-classes-final/src/main/java/com/pluralsight/Person.java
+++ week-02/day-3/intro-to-classes-final/src/main/java/com/pluralsight/Person.java
@@ -6,7 +6,27 @@ public class Person
     // each person has their own Name and Age
     private String name = "";
     private int age = 0;
+    private int energy = 100;
 
+    // constructors
+    public Person()
+    {
+        name = "";
+        age = 0;
+        energy = 100;
+    }
+
+    public Person(String name, int age)
+    {
+        // any setup logic that we want to run
+        // before the object can be used
+        this.name = name;
+        this.age = age;
+        energy = 100;
+    }
+
+
+    // getters and setters
     public String getName()
     {
         return name;
@@ -23,5 +43,22 @@ public class Person
     public void setAge(int age)
     {
         this.age = age;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    // public methods
+    public void work()
+    {
+        energy -= 10;
+        if(energy < 0) energy = 0;
+    }
+
+    public void sleep(int hours)
+    {
+        energy += (hours * 10);
+        if(energy > 100) energy = 100;
     }
 }
